f7f40940af43eeedd8ec4fa4a7a5323955111b37
Marcekace <marcelokace@gmail.com>
2023-01-24 21:05:44 -0300

    version 0.3 del juego de ajedrez con una GUI

--- Ajedrez.py
+++ Ajedrez.py
@@ -1,5 +1,5 @@
 #  Ajedrez
-#  v02 de consola
+#  v03 Basica de escritorio
 #  Copyright 2022 marcelo kacerovsky <marcelokacerovsky@marcelos-MacBook-Air.local>
 #  
 #  This program is free software; you can redistribute it and/or modify
@@ -14,29 +14,30 @@
 
 import Entidad as e
 from random import randrange
-from time import sleep
-import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import tkinter.font as tkFont
+from platform import system
 
+tablero_gui = [ [ None for j in range (11) ] for i in range(11) ]
+filas_gui = ["A", "B", "C", "D", "E", "F", "G", "H"]
 tablero = [ [ None for j in range (8) ] for i in range(8) ]
 jugador_blancas = []
 jugador_negras = []
 mmov = 0
+ind = False
+tuple_coronar = ()
 
 def mostrartablero() :
     """Muestra el tablero actualizado despues de cada ronda"""
-    print(" __________________________________________________________________________")
-    print("|   A   ||   B   ||   C   ||   D   ||   E   ||   F   ||   G   ||   H   |   |")
     for i in range(8) :
-        print("|-------++-------++-------++-------++-------++-------++-------++-------+---|")
         for j in range(8) :
             if tablero[i][j] == None :
-                print("| ", "    ", "|", end="")
+                tablero_gui[i][j].config(text="")
             else :
-                print("|  ", tablero[i][j].getIco(), "  |", end="")
-        print(" " + str(8-i) + " |", end="")
-        print("")
-    print("+-------++-------++-------++-------++-------++-------++-------++-------+---+")
-   
+                tablero_gui[i][j].config(text=tablero[i][j].getIco())
+
 def puede_mover(pieza) :
     """Toma como argumento una pieza de ajedrez y comprueba sus posibles posiciones"""
     cl = []
@@ -312,20 +313,33 @@ def puede_mover(pieza) :
                 break
     return cl
 
-def coronar(color) : 
+def coronar() : 
     """Corona un peon"""
-    while True :
-        p = input("Ingrese la pieza que va a elejir ")
-        if p.lower() == "torre" :
-            return e.Torre(color)
-        elif p.lower() == "alfil" :
-            return e.Alfil(color)
-        elif p.lower() == "reina" :
-            return e.Reina(color)
-        elif p.lower() == "caballo" :
-            return e.Caballo(color)
-        else : 
-            print("Pieza invalida.")
+    global tuple_coronar
+    eleccion = coronar_opciones.get()
+    if eleccion == "Reina" :
+        pieza = e.Reina("negro")
+    elif eleccion == "Torre" :
+        pieza = e.Torre("negro")
+    elif eleccion == "Alfil" :
+        pieza = e.Alfil("negro")
+    elif eleccion == "Caballo" :
+        pieza = e.Caballo("negro")
+    jugadorn.remove(tablero[tuple_coronar[0]][tuple_coronar[1]])
+    tablero[tuple_coronar[0]][tuple_coronar[1]] = pieza
+    pieza.setPos((tuple_coronar[0], tuple_coronar[1]))
+    jugadorn.append(pieza)
+    coronar_opciones.destroy()
+    coronar_boton.destroy()
+    campo.place(x=20, y=410, width=150)
+    boton_mover.place(x=200, y=410)
+    cartel.config(text="Ingrese su movimiento (separado por coma): ")
+    mover_m()
+    mostrartablero()
+
+def cambiarbotones() :
+    coronar_opciones.place(x=20, y=410, width=150)
+    coronar_boton.place(x=200, y =410)
 
 def coord(coord) :
     """Devuelve (pos)"""
@@ -337,28 +351,36 @@ def coord(coord) :
     except Exception :
         return None
     
+def verif_partida() :
+    """Devuelve un booleano que comprueba si termino o no la partida, buscando en el vector el rey"""
+    if reyn not in jugadorn or reyb not in jugadorb :
+        return True
+    else :
+        return False
+    
 def mover(pieza, pos) :
     """Toma como argumento una pieza de ajedrez y lo mueve en la fila y columna (tupla) que recibe como argumento"""
     c = puede_mover(pieza)
-    if hasattr(tablero[pos[0]][pos[1]], "color") :
-        if tablero[pos[0]][pos[1]].getColor() == "blanco" :
-            jugador_negras.append(tablero[pos[0]][pos[1]])
-            jugadorb.remove(tablero[pos[0]][pos[1]])
-        else :
-            jugador_blancas.append(tablero[pos[0]][pos[1]])
-            jugadorn.remove(tablero[pos[0]][pos[1]])
     if pos in c :
+        if hasattr(tablero[pos[0]][pos[1]], "color") :
+            if tablero[pos[0]][pos[1]].getColor() == "blanco" :
+                jugador_negras.append(tablero[pos[0]][pos[1]])
+                jugadorb.remove(tablero[pos[0]][pos[1]])
+            else :
+                jugador_blancas.append(tablero[pos[0]][pos[1]])
+                jugadorn.remove(tablero[pos[0]][pos[1]])
         tablero[pos[0]][pos[1]] = pieza
         tablero[pieza.getPos()[0]][pieza.getPos()[1]] = None
         pieza.setPos((pos[0], pos[1]))
+        global ind, tuple_coronar
+        ind = True
         if isinstance(pieza, e.Peon) :
             pieza.setM(False)
             if pieza.getPos()[0] == 0 and pieza.getColor() == "negro":
-                jugadorn.remove(pieza)
-                np = coronar("negro")
-                tablero[pos[0]][pos[1]] = np
-                np.setPos((pos[0], pos[1]))
-                jugadorn.append(np)
+                cambiarbotones()
+                tuple_coronar = pos
+                cartel.config(text="Elija una pieza: ")
+                ind = False
             elif pieza.getPos()[0] == 7 and pieza.getColor() == "blanco" and dosjugadores :
                 jugadorb.remove(pieza)
                 np = coronar("blanco")
@@ -373,9 +395,15 @@ def mover(pieza, pos) :
                 tablero[pos[0]][pos[1]] = m
                 m.setPos((pos[0], pos[1]))
                 jugadorb.append(m)
+        if verif_partida() :
+            if reyn not in jugadorn :
+                cartel.config(text="Has perdido!")
+                boton_mover.config(text="Salir", command=master.destroy)
+            elif reyb not in jugadorb :
+                cartel.config(text="Has ganado!")
+                boton_mover.config(text="Salir", command=master.destroy)
     else :
-        print("Movimiento invalido.")
-    os.system("clear")
+        ind = False
 
 def mover_m() :
     """Movimiento de la maquina"""
@@ -413,26 +441,85 @@ def mover_m() :
                     mover(p, c[mov])
                     mmov += 1
                     break
-    os.system("clear")
 
-def verif_partida() :
-    """Devuelve un booleano que comprueba si termino o no la partida, buscando en el vector el rey"""
-    if reyn not in jugadorn :
-        if dosjugadores :
-            print("Jugad@r B ¡ha ganado!")
-            return True
+def mov() :
+    """Mueve la pieza seleccionada por el jugador"""
+    movimiento = campo.get()
+    movimiento = movimiento.replace(","," ")
+    movimiento = movimiento.split()
+    try :
+        pieza = coord(movimiento[0])
+        pos = coord(movimiento[1])
+        mover(tablero[pieza[0]][pieza[1]],pos)
+        if ind :
+            mover_m()
+            mostrartablero()
         else :
-            print("Has perdido!")
-            return True
-    elif reyb not in jugadorb :
-        if dosjugadores :
-            print("Jugad@r N ¡ha ganado!")
-            return True
+            pass
+    except Exception :
+        pass
+    finally :
+        jb = []
+        for i in (jugador_blancas) :
+            jb.append(i.getIco())
+        jb = str(jb).replace("'", "")
+        jb = jb.replace("[", "")
+        jb = jb.replace("]", "")
+        jb = jb.replace(",", " ")
+        if len(jb) <= 22 :
+            piezas_jugblanco.config(text=jb)
         else :
-            print("Has ganado!")
-            return True
+            piezas_jugblanco2.config(text=jb[24:])
+        jn = []
+        for i in (jugador_negras) :
+            jn.append(i.getIco())
+        jn = str(jn).replace("'", "")
+        jn = jn.replace("[", "")
+        jn = jn.replace("]", "")
+        jn = jn.replace(",", " ")
+        if len(jn) <= 22 :
+            piezas_jugnegro.config(text=jn)
+        else :
+            piezas_jugnegro2.config(text=jn[24:]) 
+        campo.delete(0, tk.END)
+        mostrartablero()
+
+def acerca_de() :
+    messagebox.showinfo(title="Acerca de", message="Este juego va a dedicado a todas las personas que me apoyaron en este hermoso camino de la programacion especialmente a mi novia Romina y a mis papas Marisa y Marcelino.")
+
+def licencia() :
+    licencia = tk.Toplevel()
+    licencia.config(width=600, height=380)
+    licencia.title("Licencia")
+    licencia.resizable(0, 0)
+    if system() == "Windows" :
+        f = tkFont.Font(licencia, size=10)
     else :
-        return False
+        f = tkFont.Font(licencia, size=12)
+    l = tk.Label(licencia,
+        text="""Ajedrez 1 vs 1
+    Copyright (C) 2022  Marcelo David Kacerovsky
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact marcelokace@icloud.com""", font=f)
+    l.place(x=10, y=10)
+    b = ttk.Button(licencia, text="Aceptar", command=licencia.destroy)
+    if system() == "Windows" :
+        b.place(x=280, y=370)
+    else :
+        b.place(x=250, y=340)
 
 tb1, tb2, cb1, cb2, alfb1, alfb2, reinab, reyb = e.Torre("blanco"), e.Torre("blanco"), e.Caballo("blanco"), e.Caballo("blanco"), e.Alfil("blanco"), e.Alfil("blanco"), e.Reina("blanco"), e.Rey("blanco")
 pb1, pb2, pb3, pb4, pb5, pb6, pb7, pb8 = e.Peon("blanco"), e.Peon("blanco"), e.Peon("blanco"), e.Peon("blanco"), e.Peon("blanco"), e.Peon("blanco"), e.Peon("blanco"), e.Peon("blanco")
@@ -450,111 +537,94 @@ for i in range(8) :
     tablero[6][i] = jugadorn[i+8]
     tablero[7][i] = jugadorn[i]
 
+unjugador = True
+dosjugadores = False
 
+master = tk.Tk()
+master.title("Ajedrez v0.3")
+master.resizable(0, 0)
+color = 0
+fontstyle = tkFont.Font(size=25)
 
+menu = tk.Menu()
+menu_ayuda = tk.Menu(menu, tearoff=0)
+menu_salir = tk.Menu(menu, tearoff=0)
+menu_ayuda.add_command(label="Acerca de", command=acerca_de)
+menu_ayuda.add_command(label="Ver licencia", command=licencia)
+menu.add_cascade(label="Ayuda", menu=menu_ayuda)
+menu.add_cascade(label="Salir", menu=menu_salir, command=master.destroy)
+menu_salir.add_command(label="Salir", command=master.destroy)
 
+master.config(width=620, height=450, menu=menu)
+if system() == "Windows" :
+    master.iconbitmap('icono.ico')
+    master.config(width=650, height=500, menu=menu)
 
-while True :
-    print("###################################################")
-    print("#                                                 #")
-    print("#        Bienvenid@ elija un modo de juego        #")
-    print("#        1 - Jugar sol@                           #")
-    print("#        2 - Dos jugador@s                        #")
-    print("#        3 - Salir                                #")
-    print("#                                                 #")
-    print("###################################################")     
-    try :
-        o = int(input(">>>  "))
-        if o == 1 :
-            os.system("clear")
-            #   Un Jugador
-            unjugador = True
-            while True :
-                mostrartablero()
-                print()
-                if verif_partida() :
-                    break
-                try :
-                    while True :
-                        m = input("Ingrese su movimiento separado por coma: ")
-                        m = m.replace(",", " ")
-                        m = m.split()
-                        pos = coord(m[0])
-                        if isinstance(tablero[pos[0]][pos[1]], e.Pieza) :
-                            if tablero[pos[0]][pos[1]].getColor() == "negro" :
-                                break
-                            else :
-                                print("Debe elejir una pieza (Negra)\n\n")
-                        else :
-                            print("Debe elejir una pieza valida\n\n")
-                    mover(tablero[pos[0]][pos[1]], coord(m[1]))
-                    mostrartablero()
-                    sleep(2)
-                    if verif_partida() :
-                        break
-                    mover_m()
-                    sleep(2)
-                except Exception :
-                    os.system("clear")
-                    print("Debe elegir un movimiento valido\n\n")
-                    sleep(2)
-                    os.system("clear")
-        if o == 2 :
-            os.system("clear")
-            #   Dos jugador@s
-            dosjugadores = True
-            mostrartablero()
-            print()
-            while True :
-                if verif_partida() :
-                    break
-                while True :
-                    try :
-                        m = input("Jugad@r N ingrese su movimiento separado por coma: ")
-                        m = m.replace(",", " ")
-                        m = m.split()
-                        pos = coord(m[0])
-                        if isinstance(tablero[pos[0]][pos[1]], e.Pieza) :
-                            if tablero[pos[0]][pos[1]].getColor() == "negro" :
-                                break
-                            else :
-                                print("Debe elejir una pieza (Negra)\n\n")
-                        else :
-                            print("Debe elejir una pieza valida\n\n")
-                    except Exception :
-                        print("Debe elegir un movimiento valido\n\n")
-                        sleep(2)
-                        os.system("clear")
-                        mostrartablero()
-                mover(tablero[pos[0]][pos[1]], coord(m[1]))
-                mostrartablero()
-                if verif_partida() :
-                    break
-                while True :
-                    try :
-                        m = input("Jugad@r B ingrese su movimiento separado por coma: ")
-                        m = m.replace(",", " ")
-                        m = m.split()
-                        pos = coord(m[0])
-                        if isinstance(tablero[pos[0]][pos[1]], e.Pieza) :
-                            if tablero[pos[0]][pos[1]].getColor() == "blanco" :
-                                break
-                            else :
-                                print("Debe elejir una pieza (Blanca)\n\n")
-                        else :
-                            print("Debe elejir una pieza valida\n\n")
-                    except Exception :
-                        print("Debe elegir un movimiento valido\n\n")
-                        sleep(2)
-                        os.system("clear")
-                        mostrartablero()
-                mover(tablero[pos[0]][pos[1]], coord(m[1]))
-                mostrartablero()
-                sleep(2)
-        if o == 3 :
-            print("Gracias por jugar.")
-            break
-    except ValueError :
-        print("Debe elegir una opcion valida\n\n")
-        sleep(2)
-        os.system("clear")
+c1 = tk.Label(background="#693610")
+c1.place(x=20, y=20, width=20, height=20)
+c2 = tk.Label(background="#693610")
+c2.place(x=360, y=20, width=20, height=20)
+c3 = tk.Label(background="#693610")
+c3.place(x=20, y=360, width=20, height=22)
+c4 = tk.Label(background="#693610")
+c4.place(x=360, y=360, width=20, height=22)
+
+for i in range(1, 9) :
+    tablero_gui[0][i - 1] = tk.Label(text=filas_gui[i - 1], background="#693610")
+    tablero_gui[0][i - 1].place(x=(40 * i), y=20, width=40)
+    tablero_gui[8][i - 1] = tk.Label(text=filas_gui[i - 1],  background="#693610")
+    tablero_gui[8][i - 1].place(x=(40 * i), y=360, width=40)
+    tablero_gui[i - 1][0] = tk.Label(text=(9 - i),  background="#693610")
+    tablero_gui[i - 1][0].place(x=20, y=(40 * i), width=20, height=40)
+    tablero_gui[i - 1][10] = tk.Label(text=(9 - i),  background="#693610")
+    tablero_gui[i - 1][10].place(x=360, y=(40 * i), width=20, height=40)
+for i in range(1, 9) :
+    for j in range(1, 9) :
+        if (color + i) % 2 == 0 :
+            tablero_gui[i - 1][j - 1] = tk.Label(background="#A66023", fg="#000000", font=fontstyle)
+            tablero_gui[i - 1][j - 1].place(x=(40 * j), y=(40 * i), width=40, height=40)
+            color += 1
+        else :
+            tablero_gui[i - 1][j - 1] = tk.Label(background="#E8DDAC", fg="#000000", font=fontstyle)
+            tablero_gui[i - 1][j - 1].place(x=(40 * j), y=(40 * i), width=40, height=40)
+            color += 1
+
+cartel = tk.Label(text="Ingrese su movimiento (separado por coma): ")
+cartel.place(x=20, y=385)
+
+campo = tk.Entry()
+campo.place(x=20, y=410, width=150)
+
+boton_mover = ttk.Button(text="Mover", command=mov)
+boton_mover.place(x=200, y=410)
+
+coronar_opciones = ttk.Combobox(values=["Reina", "Torre", "Alfil", "Caballo"])
+coronar_boton = ttk.Button(text="Coronar", command=coronar)
+
+cartel_jugblanco = ttk.Label(text="Piezas capturadas:")
+cartel_jugblanco.place(x=400, y=20)
+piezas_jugblanco = ttk.Label(font=fontstyle)
+piezas_jugblanco.place(x=400, y=40, width=210, height=40)
+piezas_jugblanco2 = ttk.Label(font=fontstyle)
+piezas_jugblanco2.place(x=400, y=80, width=210, height=40)
+
+cartel_jugnegro = ttk.Label(text="Piezas capturadas:")
+cartel_jugnegro.place(x=400, y=280)
+piezas_jugnegro = ttk.Label(font=fontstyle)
+piezas_jugnegro.place(x=400, y=300, width=210, height=40)
+piezas_jugnegro2 = ttk.Label(font=fontstyle)
+piezas_jugnegro2.place(x=400, y=340, width=210, height=40)
+
+if system() == "Windows" :
+    fontwin = tkFont.Font(size=15)
+    piezas_jugblanco = ttk.Label(font=fontwin)
+    piezas_jugblanco.place(x=400, y=40, width=250, height=40)
+    piezas_jugblanco2 = ttk.Label(font=fontwin)
+    piezas_jugblanco2.place(x=400, y=80, width=250, height=40)
+    piezas_jugnegro = ttk.Label(font=fontwin)
+    piezas_jugnegro.place(x=400, y=300, width=250, height=40)
+    piezas_jugnegro2 = ttk.Label(font=fontwin)
+    piezas_jugnegro2.place(x=400, y=340, width=250, height=40)
+
+mostrartablero()
+master.mainloop()
